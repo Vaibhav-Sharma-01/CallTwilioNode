de80b248bedf341bfd6225b1337b86cae78727e1
Vaibhav-Sharma-01 <thegreatvaibhavsharma@gmail.com>
2021-03-27 19:07:00 +0530

    callapideploy

---
+++ index.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const express = require('express');
+const {urlencoded} = require('body-parser');
+const twilio = require('twilio');
+const ClientCapability = twilio.jwt.ClientCapability;
+const VoiceResponse = twilio.twiml.VoiceResponse;
+
+
+const app = express();
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/public'));
+app.use(urlencoded({extended: false}));
+
+// Generate a Twilio Client capability token
+app.get('/token', (request, response) => {
+  const capability = new ClientCapability({
+    accountSid: process.env.TWILIO_ACCOUNT_SID,
+    authToken: process.env.TWILIO_AUTH_TOKEN
+  });
+
+  capability.addScope(
+    new ClientCapability.OutgoingClientScope({
+      applicationSid: process.env.TWILIO_TWIML_APP_SID})
+  );
+
+  const token = capability.toJwt();
+
+  // Include token in a JSON response
+  response.send({
+    token: token,
+  });
+});
+
+// Create TwiML for outbound calls
+app.post('/voice', (request, response) => {
+  const voiceResponse = new VoiceResponse();
+  voiceResponse.dial({
+    callerId: process.env.TWILIO_NUMBER,
+  }, request.body.number);
+
+  response.type('text/xml');
+  response.send(voiceResponse.toString());
+});
+
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Express Server listening on *:${port}`);
+});
+
+module.exports = app;
